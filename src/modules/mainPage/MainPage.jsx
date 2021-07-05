5bf30b6eb12910a6cd0120f88f4fa8ba8c59fc6a
shamil95 <shamil.isayev95@gmail.com>
2021-07-05 19:19:34 +0400

    ISI

--- src/modules/mainPage/MainPage.jsx
+++ src/modules/mainPage/MainPage.jsx
@@ -1,38 +1,50 @@
 import axios from 'axios';
-import React, { useState } from 'react';
-import QrReader from 'react-qr-reader'
+import React, { useState, useEffect } from 'react';
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 const MainPage = () => {
-  const[result, setResult] = useState('');
+
+  const [data, setData] = useState("");
+  const [music, setMusic] = useState('');
+
+  useEffect(() => {
+    axios.post('https://httpbin.org/anything', data).then((response) => {
+      setMusic(response)
+    }, (error) => {
+      console.log(error);
+    });;
+    
+  }, [data])
+
+   
   
-  const handleScan = (data) => {
-    setResult(data)
-  }
+  return (
+    <div className="App">
+      {
+        !data ?       <header className="App-header">
+        <BarcodeScannerComponent
+            width={500}
+            height={500}
+            onUpdate={(err, result) =>  {
+                console.log(err, result);
+                if (result) setData(result.text)
+            }}
+        />
+              <div className='line'>
+            
+              </div>
+      </header>
+      : <div>
+        <div>{data}</div>
+              <audio controls>
+      <source src={music} type="audio/mpeg" />
+      </audio>
+      </div>
 
-  const handleError = (err) => {
-    console.log(err);
-  }
+      }
 
-  const sendQr = () => {
-    axios.post('https://httpbin.org/anything', result)
-  }
-
-  return(
-    <div>
-      <div style={{width: '300px'}}>
-      <QrReader
-        delay={100}
-        onError={handleError}
-        onScan={handleScan}
-      >
-      </QrReader>
     </div>
-    <div>
-      <button onClick={sendQr}>SEND DATA</button>
-      {result}
-    </div>
-    </div>
-  )
+  );
 }
 
 export default React.memo(MainPage);
